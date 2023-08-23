fa4ee1ae524912fb10a790c558760d8cd80d374c
TALHS16 <100534541+TALHS16@users.noreply.github.com>
2023-08-23 15:21:13 +0300

    optimaize and blur fix

--- camera/python/deparity_map.py
+++ camera/python/deparity_map.py
@@ -2,87 +2,35 @@ import csv
 import cv2
 import numpy as np
 from visualisation import visualize_point_cloud
+from numba import jit, uint8
 
-def find_Disparity(left, right, window_size, threshold, median_size):
 
-    # with open('set_'+str(n)+'/max_disp.txt', 'r') as f:
-    #     max_disparity = f.readline()
-    #     max_disparity = int(max_disparity)
+def find_Disparity(left, right, window_size, median_size):
+    cost_volume = part1(left, right, window_size, median_size)
+    aggregated_cost = np.empty_like(cost_volume)
+    for d in range(31):
+        aggregated_cost[:, window_size//2:, d] = cv2.medianBlur(cost_volume[:,window_size//2:,d], median_size)
+    return np.argmin(aggregated_cost, axis=2)
+
+@jit(uint8[:,:,:](uint8[:, :], uint8[:, :], uint8, uint8), nopython=True, fastmath=True)
+def part1(left, right, window_size, median_size):
     max_disparity = 31
-    # left_census = np.zeros_like(left, dtype=np.ndarray)
-    # right_census = np.zeros_like(right, dtype=np.ndarray)
+    left_census = np.empty((left.shape[0], left.shape[1], window_size**2), dtype=np.uint8)
+    right_census = np.empty((right.shape[0], right.shape[1], window_size**2), dtype=np.uint8)
+    for i in range(window_size // 2, left.shape[0] - window_size // 2):
+        for j in range(window_size // 2, left.shape[1] - window_size // 2):
+            left_window = left[i - window_size // 2:i + window_size // 2 + 1,
+                          j - window_size // 2:j + window_size // 2 + 1]
+            right_window = right[i - window_size // 2:i + window_size // 2 + 1,
+                           j - window_size // 2:j + window_size // 2 + 1]
+            left_census[i, j] = (left_window >= left[i, j]).ravel()
+            right_census[i, j] = (right_window >= right[i, j]).ravel()
 
-    left_census = np.ndarray(shape=(left.shape[0], left.shape[1], window_size[0] * window_size[1]))#, dtype=bool)
-    right_census = np.ndarray(shape=(right.shape[0], right.shape[1], window_size[0] * window_size[1]))#, dtype=bool)
-    for i in range(window_size[0] // 2, left.shape[0] - window_size[0] // 2):
-        for j in range(window_size[1] // 2, left.shape[1] - window_size[1] // 2):
-            left_window = left[i - window_size[0] // 2:i + window_size[0] // 2 + 1,
-                          j - window_size[1] // 2:j + window_size[1] // 2 + 1]
-            right_window = right[i - window_size[0] // 2:i + window_size[0] // 2 + 1,
-                           j - window_size[1] // 2:j + window_size[1] // 2 + 1]
-
-            left_census[i, j] = np.array([left_window >= left[i, j]]).flatten()
-            right_census[i, j] = np.array([right_window >= right[i, j]]).ravel()
-
-    left_census = left_census.astype(np.uint8)
-    right_census = right_census.astype(np.uint8)
-
-    cost_volume = np.zeros((left.shape[0], left.shape[1], max_disparity),dtype=np.uint8)
-    inverse_cost_volume = np.zeros_like(cost_volume)
-    for d in range(max_disparity):
-        for y in range(window_size[0] // 2, left.shape[0] - window_size[0] // 2): #--
-            for x in range(window_size[1] // 2, left.shape[1] - window_size[1] // 2):  #--
-
-                if x - d >= (window_size[1] // 2):  #0--
-                    cost_volume[y, x, d] = np.count_nonzero(left_census[y, x] != right_census[y, x - d])
-                if x+d < (left.shape[1] - window_size[1]// 2): #left.shape[1]--
-                    inverse_cost_volume[y, x, d] = np.count_nonzero(left_census[y, x+d] != right_census[y, x])
-
-    # Apply aggregation
-    # disparity = cv2.medianBlur(initial_disparity.astype(np.uint8), median_size)
-    # disparity_inv = cv2.medianBlur(initial_disparity_inverse.astype(np.uint8), median_size)
-
-    aggregated_cost = np.zeros_like(cost_volume)
-    aggregated_cost_inv = np.zeros_like(cost_volume)
-    for d in range(max_disparity):
-        aggregated_cost[:, window_size[1]//2:, d] = cv2.medianBlur(cost_volume[:,window_size[1]//2:,d], median_size)
-        aggregated_cost_inv[:,:left.shape[1]-window_size[1]//2, d] = cv2.medianBlur(inverse_cost_volume[:,:left.shape[1]-window_size[1]//2,d], median_size)
-      
-    # disparity = cost_aggregation(cost_volume, median_size)
-    # disparity_inv = cost_aggregation(inverse_cost_volume, median_size)
-
-    disparity = np.argmin(aggregated_cost, axis=2)
-    disparity_inv = np.argmin(aggregated_cost_inv, axis=2)
-    #disparity[:, :window_size[1]//2] = disparity_inv[:, left.shape[1]-window_size[1]//2-1:] =disparity_inv[:, :window_size[1]//2]= 0
-
-    #Compute the consistency check and filter the initial disparity map
-    for i in range(window_size[0]//2, left.shape[0]-window_size[0]//2):
-        for j in range(window_size[1]//2, left.shape[1]-window_size[1]//2):
-            d = disparity[i, j]
-            if j - d >= 0:
-                consistency_check_l = abs(d - disparity_inv[i, j - d]) <= threshold
-                if not consistency_check_l:
-                    disparity[i, j] = 0
-
-            d = disparity_inv[i, j]
-            if j + d < right.shape[1]:
-                consistency_check_r = abs(d - disparity[i, j + d]) <= threshold
-                if not consistency_check_r:
-                    disparity_inv[i, j] = 0
-
-
-
-
-    disparity_normalized = ((disparity - np.min(disparity)) / (np.max(disparity) - np.min(disparity)) * 255).astype( np.uint8)
-    disparity_inv_normalized = ((disparity_inv - np.min(disparity_inv)) / (np.max(disparity_inv) - np.min(disparity_inv)) * 255).astype( np.uint8)
-    # cv2.imshow('Disparity  left', disparity_normalized)
-    # cv2.imshow('Disparity  right', disparity_inv_normalized)
-
-    # save the display mood of norm values- to submit results!!
-    # cv2.imwrite('disp_left_n.jpg',disparity_normalized)
-    # cv2.imwrite('disp_right_n.jpg',disparity_inv_normalized)
-
-    return disparity, disparity_inv
+    cost_volume = np.empty((left.shape[0], left.shape[1], max_disparity),dtype=np.uint8)
+    cost_volume[:, :, 0] = np.sum(left_census[:, :] != right_census[:, :], axis=2)
+    for d in range(1, max_disparity):
+        cost_volume[d:, :, d] = np.sum(left_census[:-d, :] != right_census[d:, :], axis=2)
+    return cost_volume
 
 def compute_depth_map(disparity_map, baseline, focal_length):
 
@@ -98,12 +46,12 @@ def generate_point_cloud(disparity_map, baseline, focal_length):
     cloud[:, :, 1] = (np.arange(cloud.shape[0]*cloud.shape[1]).reshape(cloud.shape[1], cloud.shape[0]) % cloud.shape[0]).T
     cloud[:, :, 2] = baseline*focal_length/disparity_map
 
-    # remove outlires
+    # remove outliers
     cloud = cloud[np.isfinite(cloud[:, :, 2])]
     threshold = np.percentile(cloud[:, 2], 70)
     cloud = cloud[cloud[:, 2] < threshold]
-    cloud[:, 0]  = (cloud[:, 0]-cols/2)/focal_length*cloud[:, 2]
-    cloud[:, 1]  = (cloud[:, 1]-rows/2)/focal_length*cloud[:, 2]
+    cloud[:, 0]  = -(cloud[:, 0]-cols/2)/focal_length*cloud[:, 2]
+    cloud[:, 1]  = -(cloud[:, 1]-rows/2)/focal_length*cloud[:, 2]
     return cloud
 
 if __name__ == '__main__':
@@ -113,54 +61,55 @@ if __name__ == '__main__':
     for i in range(40):
         cap.read()
     ret1,leftImage = cap.read()
-    leftImage = cv2.rotate(leftImage, cv2.ROTATE_90_CLOCKWISE)
+    # leftImage = cv2.rotate(leftImage, cv2.ROTATE_90_CLOCKWISE)
     #slice thr human in image
     cv2.imwrite("left.png",leftImage)
-    leftImage = leftImage[250:400,0:400]
+    # leftImage = leftImage[250:400,0:400]
     
     for i in range(8):
         cap.read()
     ret2,rightImage = cap.read()
-    rightImage = cv2.rotate(rightImage, cv2.ROTATE_90_CLOCKWISE)
+    # rightImage = cv2.rotate(rightImage, cv2.ROTATE_90_CLOCKWISE)
     #slice thr human in image
     cv2.imwrite("right.png",rightImage)
-    rightImage = rightImage[250:400,0:400]
+    # rightImage = rightImage[250:400,0:400]
     camera_matrix = np.loadtxt('../data/K.txt')
     focal_length = camera_matrix[0][0]
 
     left = cv2.cvtColor(leftImage, cv2.COLOR_BGR2GRAY)
     right = cv2.cvtColor(rightImage, cv2.COLOR_BGR2GRAY)
-
+    # left = cv2.resize(left, (640, 480))
+    # right = cv2.resize(right, (640, 480))
     #Var for disparty map
     baseline = 2
-    window_size=[7, 7]
+    window_size=7
     threshold = 0.9
     median_size = 13 #remove noise
 
 
 
-    disparity, disparity_inv = find_Disparity(left, right, window_size, threshold, median_size)
+    disparity = find_Disparity(left, right, window_size, median_size)
+    blur_kernel_size = (5, 5)  
+    sigma = 0.8
 
+    disparity = cv2.GaussianBlur(disparity.astype(np.float32), blur_kernel_size, sigma)
+    
     depth_left = compute_depth_map(disparity, baseline, focal_length)
-    depth_right = compute_depth_map(disparity_inv, baseline, focal_length)
+    # depth_right = compute_depth_map(disparity_inv, baseline, focal_length)
 
     depth_left = depth_left.astype(np.uint8)
-    depth_right = depth_right.astype(np.uint8)
+    # depth_right = depth_right.astype(np.uint8)
 
     cv2.imwrite('depth_left.jpg', depth_left)
-    cv2.imwrite('depth_right.jpg',depth_right)
+    # cv2.imwrite('depth_right.jpg',depth_right)
 
     point_cloud = generate_point_cloud(disparity, baseline, focal_length)
 
 
 
-    point_cloud[:,2:] = point_cloud[:,2:] / 3
+    # point_cloud[:,2:] = point_cloud[:,2:] / 3
 
     #bulr z- value for more filled image
-    blur_kernel_size = (5, 5)  
-    sigma = 0.8
-
-    point_cloud[:,2:] = cv2.GaussianBlur(point_cloud[:,2:], blur_kernel_size, sigma)
     
     visualize_point_cloud(point_cloud)
 
