eb5ae714155783779317e6b8174608ae47006bba
TALHS16 <100534541+TALHS16@users.noreply.github.com>
2023-08-30 20:10:42 +0300

    delete non required files

--- camera/python/setup.py
+++
@@ -1,4 +0,0 @@
-from setuptools import setup, Extension
-from Cython.Build import cythonize
-
-setup(ext_modules=cythonize([Extension('dismap', ['dismap.pyx'], extra_compile_args=["-O3", '-march=native', '-mtune=native', '-fsigned-char'])], annoate=True, langauge_level="3", compiler_directives={"boundscheck":False, "wraparound": False, "cdivision": True}))
